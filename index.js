ecaba4d585c9daad60ccb7dfbc80ba145e21e880
MrPancakes39 <salmannhassan39@gmail.com>
2020-06-12 17:29:19 +0300

    Added security.txt to the App

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening at port ${port}`));
 app.use(express.static("public"));
+
+app.get("/.well-known/security.txt", (req, res) => {
+    res.sendFile(__dirname + "/docs/security.txt");
+});
+
+app.get("/security.txt", (req, res) => {
+    res.sendFile(__dirname + "/docs/security.txt");
+});
